6edacbbe30baca3e7974c2a81bfe1e1630b4f057
Satyam <satyambhardawaj@gmail.com>
2020-10-17 09:28:45 +0530

    search date functionality

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Banner from './Banner';
 import './Home.css';
+
 function Home() {
     return (
         <div className="home">
-            
+            <Banner />
         </div>
     )
 }
