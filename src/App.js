9ce92a7d262fe787c9946974f8d14f1c3e70ebcd
Satyam <satyambhardawaj@gmail.com>
2020-10-18 12:35:04 +0530

    home page complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import './App.css';
+import Footer from './Footer';
 import Header from './Header';
 import Home from './Home';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import SearchPage from './SearchPage';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
-        {/* header */}
-        {/* Banner */}
-          {/* search */}
-        {/* cards */}
-        {/* footer */}
-      {/* SearchPage */}
-        {/* ..... */}
+      
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+          
+        </Switch>
+      </Router>
+      
+      <Footer />
+        
     </div>
   );
 }
